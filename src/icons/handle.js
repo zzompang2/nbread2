3c3f039b75887745da8508ef05fb145c3fcba15e
Ham Ham <ham@Hamui-MacBookPro.local>
2021-06-26 10:41:55 +0900

    hover-space 가 input 등을 막는 문제 해결

--- src/icons/handle.js
+++ src/icons/handle.js
@@ -1,4 +1,4 @@
-export function Handle({ color = "red" }) {
+export function Handle() {
   return (
     <svg
       width="4"
@@ -6,7 +6,6 @@ export function Handle({ color = "red" }) {
       viewBox="0 0 4 24"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ color: color }}
     >
       <rect width="4" height="24" rx="2" />
     </svg>
